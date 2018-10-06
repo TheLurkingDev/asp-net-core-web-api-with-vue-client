97279888ee2f939b3fff5960fd617da88d9cd1c3
Darren Cook <thelurkingdev@gmail.com>
2018-10-06 14:45:25 -0400

    Added axios.

--- AccountOwnerClient/src/main.js
+++ AccountOwnerClient/src/main.js
@@ -6,8 +6,12 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Axios from 'axios';
 
 Vue.config.productionTip = false;
+
+// 'process' is a global Node.js variable.
+Axios.defaults.baseURL = process.env.API_ENDPOINT;
 
 Vue.use(BootstrapVue);
 
