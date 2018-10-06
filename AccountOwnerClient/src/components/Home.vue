d543871f7c9276cf26cda3703313b02e8f01ca7a
Darren Cook <thelurkingdev@gmail.com>
2018-10-06 13:46:33 -0400

    Added Home.vue. Restructured App.vue. Setup routing to Home.vue.

---
+++ AccountOwnerClient/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <p class="home-text">
+        WELCOME TO THE ACCOUNT-OWNER APPLICATION
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+    .home-text {
+        font-size: 35px;
+        color: red;
+        text-align: center;
+        position: relative;
+        top: 30px;
+        text-shadow: 2px 2px 2px gray;
+    }
+</style>
